d8a61480d60fdbb0366247c4707ae9db401ce489
Daniel Agar <daniel@agar.ca>
2021-09-14 19:58:59 -0400

    mavlink module generate improvements

--- src/modules/mavlink/mavlink_receiver.cpp
+++ src/modules/mavlink/mavlink_receiver.cpp
@@ -196,9 +196,12 @@ MavlinkReceiver::handle_message(mavlink_message_t *msg)
 		handle_message_landing_target(msg);
 		break;
 
+#if defined(MAVLINK_MSG_ID_CELLULAR_STATUS)
+
 	case MAVLINK_MSG_ID_CELLULAR_STATUS:
 		handle_message_cellular_status(msg);
 		break;
+#endif // MAVLINK_MSG_ID_CELLULAR_STATUS
 
 	case MAVLINK_MSG_ID_ADSB_VEHICLE:
 		handle_message_adsb_vehicle(msg);
@@ -2430,6 +2433,7 @@ MavlinkReceiver::handle_message_landing_target(mavlink_message_t *msg)
 	}
 }
 
+#if defined(MAVLINK_MSG_ID_CELLULAR_STATUS)
 void
 MavlinkReceiver::handle_message_cellular_status(mavlink_message_t *msg)
 {
@@ -2449,6 +2453,7 @@ MavlinkReceiver::handle_message_cellular_status(mavlink_message_t *msg)
 
 	_cellular_status_pub.publish(cellular_status);
 }
+#endif // MAVLINK_MSG_ID_CELLULAR_STATUS
 
 void
 MavlinkReceiver::handle_message_adsb_vehicle(mavlink_message_t *msg)
